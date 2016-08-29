fa9bcdfad72bf608975b931dd8ded3b706003e10
ProfessorFluffy <nbunzli@gmail.com>
2016-08-28 17:03:47 -0700

    Add files via upload
    Adding assets (1 of 2)

---
+++ Assets/Editor/DungeonGeneratorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DungeonGenerator))]
+public class DungeonGeneratorEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		DungeonGenerator myScript = (DungeonGenerator)target;
+		if(GUILayout.Button("Generate Dungeon"))
+		{
+			myScript.GenerateDungeon();
+		}
+	}
+}
